844ebf7533d959bc189ca6992d29bc8657719d8b
shubh182002 <shubhamkinkar9@gmail.com>
2024-10-22 16:57:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className=" bg-red-500 justify-center text-center p-10 m-10">
+      shubham
+     
+    </div>
+  );
+}
+
+export default App;
